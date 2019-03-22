0cd2b550b31a4113eb8475dda960a918454b37a8
Carly Pecora <carly@carlypecora.com>
2019-03-22 10:42:22 -0400

    Fix bugs, clean up code * remove excess lines * remove commented code * change width and height to 100% for a few pages * fix off-by-one error in gallery

--- src/carlyAmeling.js
+++ src/carlyAmeling.js
@@ -1,41 +1,13 @@
-// showLogo();
 init();
-// showLogo();
-// hideLogo();
-
 
 function init() {
-	// logo();
 	mapNavToPage();
 	homeBtn();
 	slideShow();
 	expandPic();
 	closeExpandedPic();
 	showHeader();
-	
 }
-
-// function logo() {
-// 	$(".logo").css("display", "block");
-// 	$(".container").css("display", "none");
-// 	$(".ameling").css("display", "none");
-// 	setInterval(function(){
-// 		$(".logo").fadeOut(4000);
-// 		$(".container").fadeIn(4000);
-// 	});
-// 	return;
-// }
-
-// function showLogo(){
-// 		$("#logo").css("display", "block");
-// 		$(".container").css("display", "none");
-// 		$(".ameling").css("display", "none");
-// }
-
-// function hideLogo(){
-// 	window.clearTimeout(openingPg, 3000);
-// } 
-
 
 function mapNavToPage(){
 	var items = document.querySelectorAll(".navbar-brand");
@@ -50,25 +22,14 @@ function mapNavToPage(){
 				if (content[i].id === clickedItemLower){
 					content[i].style.display = "block";
 					firstPage.style.display = "none";
-
 				} else {
 					content[i].style.display = "none";
-					// firstPage.style.display = "block";
 				}
 			}
 		})
 	}
 }
 
-// function showHeader(){
-// 	$(".navbar-brand").on("click", function(){
-// 		if(this === $(".galleryNav")){
-// 			$("header").slideDown(1000);
-// 			// $("header").show();
-// 		} else {
-// 			$("header").slideUp(1000);
-// 		}
-// 	})
 function showHeader(){
  $(".gal").on("click", function(){
  	$("header").slideDown(1000);
@@ -81,28 +42,12 @@ function homeBtn(){
 	var homeBtn = document.querySelector(".firstNav");
 		$(navBtns).on("click", function(){
 			if(this !== homeBtn){
-				// $("header").slideUp(1000);
 				$(".firstPage").css("display", "none");
-				// $(".headshot").hide();
 			} else {
-				// $("header").slideDown(1000);
 				$(".firstPage").css("display", "block");
-
-			};
-		});
+		};
+	});
 }
-
-// function homeBtn(){
-// 	var fullNav = document.querySelectorAll(".navbar-brand");
-// 	var homeBtn = document.querySelector(".firstNav");
-// 	fullNav.addEventListener("click", function(){
-// 		if(this === homeBtn) {
-// 			$(".firstPage").css("display", "block");
-// 		} else {
-// 			$(".firstPage").css("display", "none");
-// 		}
-// 	})
-// }
 
 function slideShow(){
 	var headshots = document.getElementsByClassName('headshot');
@@ -128,7 +73,7 @@ function expandPic(){
 				if (j !== i) {
 					otherPics.push(pictures[j])
 				};
-				otherPics.push(pictures[5]) // WHAT THE FUCK?!!!?!?
+				otherPics.push(pictures[6]) // WHAT THE FUCK?!!!?!?
 			};
 			$(otherPics).hide();
 			$(".expanded").fadeIn(250);
@@ -138,12 +83,9 @@ function expandPic(){
 };
 
 function closeExpandedPic(){	
-	// When the user clicks on <span> (x), close the modal
-	// and show the image gallery again
 	$(".close").on("click", function(){
 		$(".foo").removeClass("expanded");
   		$(".foo").show();
-  		// $(".foo").css("border:hover", "3px solid #911b4a");
   		$(".close").css("display", "none");
   	})
 }
@@ -152,13 +94,3 @@ $('.fa-caret-down').click(function() {
       $("header").slideUp();
       $("header").show();	
 });
-
-
-// amelingWebsite.listen(3000)
-
-// $("html, body").animate({
-//     scrollTop: $(document).height()
-// }, 1000);
-
-
-
